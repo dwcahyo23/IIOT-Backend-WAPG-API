9eeef96464da38ac99626211792df8c87374f926
Yusuf Dwi <dwcahyo23@gmail.com>
2023-08-04 13:37:53 +0700

    #040823

--- src/apps/Mn-Wo.js
+++ src/apps/Mn-Wo.js
@@ -190,7 +190,7 @@ export default {
           Sequelize.where(
             Sequelize.fn('date', Sequelize.col('ymd')),
             '>=',
-            format(new Date(), 'yyyy-MM-dd'),
+            '2023-08-01',
           ),
           {
             chk_mark: {
