953a1f11ce28ef96bdc8fdb516219e787c41ea8e
Yusuf Dwi <dwcahyo23@gmail.com>
2023-08-11 15:16:11 +0700

    #110823

--- src/apps/Qc-Lock.js
+++ src/apps/Qc-Lock.js
@@ -48,7 +48,7 @@ export default {
     where       
       sts_wa = 'N' 
       and
-	date(sch_ot.mad_qap_locm.ymd) >= '2023-07-01'
+	date(sch_ot.mad_qap_locm.ymd) = CURRENT_DATE
     order by 
       sch_ot.mad_qap_locm.ymd asc`,
       { type: QueryTypes.SELECT },
