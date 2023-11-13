a22d2a43a338c275fb8ecdada28d65d22705c61b
Yusuf Dwi <dwcahyo23@gmail.com>
2023-11-13 10:48:19 +0700

    #131123

--- src/apps/scheduler/index.js
+++ src/apps/scheduler/index.js
@@ -19,29 +19,29 @@ export default {
         .getClose()
         .then((res) => console.log(res))
         .catch((err) => console.log(err))
-      QcLock.getLock().then((res) => console.log(res))
+      // QcLock.getLock().then((res) => console.log(res))
     })
 
     // //! Job Scheduler EWS 2H
-    const ews1 = schedule.scheduleJob('1 0 8 * * *', function () {
-      Ews.getCritical().then((res) => console.log(res))
-    })
+    // const ews1 = schedule.scheduleJob('1 0 8 * * *', function () {
+    //   Ews.getCritical().then((res) => console.log(res))
+    // })
 
-    const ews2 = schedule.scheduleJob('1 0 10 * * *', function () {
-      Ews.getCritical().then((res) => console.log(res))
-    })
+    // const ews2 = schedule.scheduleJob('1 0 10 * * *', function () {
+    //   Ews.getCritical().then((res) => console.log(res))
+    // })
 
-    const ews3 = schedule.scheduleJob('1 0 13 * * *', function () {
-      Ews.getCritical().then((res) => console.log(res))
-    })
+    // const ews3 = schedule.scheduleJob('1 0 13 * * *', function () {
+    //   Ews.getCritical().then((res) => console.log(res))
+    // })
 
-    const ews4 = schedule.scheduleJob('1 0 15 * * *', function () {
-      Ews.getCritical().then((res) => console.log(res))
-    })
+    // const ews4 = schedule.scheduleJob('1 0 15 * * *', function () {
+    //   Ews.getCritical().then((res) => console.log(res))
+    // })
 
-    const ews5 = schedule.scheduleJob('1 0 17 * * *', function () {
-      Ews.getCritical().then((res) => console.log(res))
-    })
+    // const ews5 = schedule.scheduleJob('1 0 17 * * *', function () {
+    //   Ews.getCritical().then((res) => console.log(res))
+    // })
 
     //! Job Scheduler PR
     // const jobPPUMorning = schedule.scheduleJob('1 0 8 * * 1-5', function () {
